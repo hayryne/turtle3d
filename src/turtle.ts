af7994a951124125d60e420f01ca291d537ae5df
hayryne <mikko.hayrynen@hotmail.com>
2019-11-30 16:20:26 +0200

    general cleanup

--- src/turtle.ts
+++ src/turtle.ts
@@ -1,82 +1,96 @@
 
-import { Engine } from '@babylonjs/core/Engines/engine';
-import { Scene } from '@babylonjs/core/scene';
-import { Vector3 } from '@babylonjs/core/Maths/math';
-import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera';
-import { Mesh } from '@babylonjs/core/Meshes/mesh';
-import { LinesMesh } from '@babylonjs/core/Meshes/linesMesh';
+import { Engine } from '@babylonjs/core/Engines/engine'
+import { Scene } from '@babylonjs/core/scene'
+import { Vector3 } from '@babylonjs/core/Maths/math'
+import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera'
+import { Mesh } from '@babylonjs/core/Meshes/mesh'
+import { LinesMesh } from '@babylonjs/core/Meshes/linesMesh'
 
 // side-effects only imports allowing Mesh to create default shapes
 import '@babylonjs/core/Meshes/meshBuilder'
-import '@babylonjs/core/Materials/standardMaterial';
-
-const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-const engine = new Engine(canvas);
-var scene = new Scene(engine);
-
-var camera = new FreeCamera('camera', new Vector3(100, 75, 100), scene);
-
-camera.setTarget(Vector3.Zero())
-camera.attachControl(canvas, true)
-
-engine.runRenderLoop(() => scene.render())
+import '@babylonjs/core/Materials/standardMaterial'
 
 enum Direction { Horizontal, Vertical }
 
-let actions = [];
+const createScene = () : Scene => {
+  const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement
+  const engine = new Engine(canvas)
+  const scene = new Scene(engine)
 
-let currentZenithAngle = 0;
-let currentAzimuthAngle = 0;
+  const camera = new FreeCamera('camera', new Vector3(100, 75, 100), scene)
 
-let currentPosition = new Vector3(0, 0, 0)
-let targetPosition = new Vector3(0, 0, 0)
+  camera.setTarget(Vector3.Zero())
+  camera.attachControl(canvas, true)
 
-const createLine = (pos1 : Vector3, pos2 : Vector3) => Mesh.CreateTube('line', [pos1, pos2], 0.5, 10, null, null, scene)
+  engine.runRenderLoop(() => scene.render())
 
-function walkTowardsCurrentDestination() : void {
-    var towardsNew = targetPosition.subtract(currentPosition).normalize()
+  return scene
+}
 
-    createLine(currentPosition, currentPosition.add(towardsNew));
-    currentPosition = currentPosition.add(towardsNew);
+const scene = createScene()
 
-    if (!currentPosition.equalsWithEpsilon(targetPosition, 0.01)) {
-        queueAction(() => walkTowardsCurrentDestination())
-    } else {
-        executeNextAction()
-    }
+const actions = [];
+
+const currentAngles = { zenith: 0, azimuth: 0 }
+const positions = { current: Vector3.Zero(), destination: Vector3.Zero() }
+
+const createLine = (pos1 : Vector3, pos2 : Vector3) =>
+  Mesh.CreateTube('line', [pos1, pos2], 0.5, 10, null, null, scene)
+
+const walkTowardsCurrentDestination = () : void => {
+  const towardsNew = positions.destination.subtract(positions.current).normalize()
+  const nextPosition = positions.current.add(towardsNew)
+
+  createLine(positions.current, nextPosition)
+  positions.current = nextPosition
+
+  const destinationReached = positions.current.equalsWithEpsilon(positions.destination, 0.01)
+
+  if (!destinationReached) {
+    queueAction(() => walkTowardsCurrentDestination())
+  } else {
+    executeNextAction()
+  }
 }
 
 const walk = (distance : number) => {
-    actions.push(() => {
-      getNextTargetPosition(distance)
-      walkTowardsCurrentDestination()
-    })
+  const walkAction = () => {
+    setNextTargetPosition(distance)
+    walkTowardsCurrentDestination()
+  }
+
+  actions.push(walkAction)
 }
 
-function getNextTargetPosition(distance : number) {
-    let position = new Vector3(
-        distance * Math.sin(currentAzimuthAngle) * Math.cos(currentZenithAngle),
-        distance * Math.sin(currentZenithAngle),
-        distance * Math.cos(currentAzimuthAngle) * Math.cos(currentZenithAngle)
-    )
+const setNextTargetPosition = (distance : number) => {
+  let position = new Vector3(
+    distance * Math.sin(currentAngles.azimuth) * Math.cos(currentAngles.zenith),
+    distance * Math.sin(currentAngles.zenith),
+    distance * Math.cos(currentAngles.azimuth) * Math.cos(currentAngles.zenith)
+  )
 
-    targetPosition = currentPosition.add(position)
+  positions.destination = positions.current.add(position)
 }
 
-const queueAction = (action : Function) => setTimeout(action)
+const queueAction = (action : Function) => setTimeout(action, 25)
 
-const turn = (turnAngle : number, direction : Direction) =>
-  actions.push(() => queueAction(() => {
+const turn = (turnAngle : number, direction : Direction) => {
+  const turnAction = () => {
+    const radians = turnAngle * Math.PI / 180
+
     if (direction === Direction.Horizontal) {
-        currentAzimuthAngle += turnAngle * Math.PI / 180
+      currentAngles.azimuth += radians
     }
 
     if (direction === Direction.Vertical) {
-        currentZenithAngle += turnAngle * Math.PI / 180
+      currentAngles.zenith += radians
     }
 
     executeNextAction()
-}))
+  }
+
+  actions.push(turnAction)
+}
 
 const turnHorizontal = (angle : number) => turn(angle, Direction.Horizontal)
 const turnVertical = (angle : number) => turn(angle, Direction.Vertical)
